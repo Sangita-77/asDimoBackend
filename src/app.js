3523c0cdbf724b2eb5f9a717854e2574b3bf0aa5
Sangita Singh <sangita@dreamlogodesign.com>
2026-03-16 16:23:52 +0530

    add security helmet

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import routes from "./routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(helmet());
 
 app.use(cors());
 app.use(express.json());
