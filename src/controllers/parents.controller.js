4b4ef63dacc68eecfe1a3fb4f689c441d9c4b191
Sangita Singh <sangita@dreamlogodesign.com>
2026-03-16 12:31:52 +0530

    assesment test

--- src/controllers/parents.controller.js
+++ src/controllers/parents.controller.js
@@ -7,20 +7,20 @@ import {
 
 export const assesmentTestCon = asyncHandler(async (req , res) => {
 
-    const { parentId, answers } = req.body;
-  
-    if (!parentId || !answers ) {
-      return res.status(400).json({
-        success: false,
-        message: "Please provide parentId, answers ",
-      });
-    }
-  
-    const testData = await assetmentTestService(req.body);
-  
-    res.status(200).json({
-      success: true,
-      data: testData,
+  const { parentId, answers } = req.body;
+
+  if (!parentId || !answers ) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide parentId, answers ",
     });
-  
+  }
+
+  const testData = await assetmentTestService(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: testData,
   });
+
+});
