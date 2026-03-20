6a8c660ac93e3dae898e81a662c5bcaaa7fcde32
Sangita Singh <sangita@dreamlogodesign.com>
2026-03-20 14:55:44 +0530

    add authentication and protection middleware to needed routes.....

--- src/routes.js
+++ src/routes.js
@@ -11,22 +11,22 @@ const router = Router();
 // Authentication routes
 router.post("/v1/auth/register", register);
 router.post("/v1/auth/login", login);
-router.get("/v1/auth/profile", authenticate, getProfile);
+router.get("/v1/auth/profile", authenticate, protect , getProfile);
 router.get("/v1/auth/getAllUsers", getAllUsers);
 
 router.post("/v1/auth/logout", protect, logout);
 
 // AssesmentSet routes
-router.post("/v1/addAssesment", authenticate, addAssesment);
-router.get("/v1/getAssesment", authenticate, getAssesment);
+router.post("/v1/addAssesment", authenticate, protect , addAssesment);
+router.get("/v1/getAssesment", authenticate, protect , getAssesment);
 
 // parents
 
-router.post("/v1/assesmentTest", authenticate, assesmentTestCon);
+router.post("/v1/assesmentTest", authenticate, protect , assesmentTestCon);
 
 // teachers
 
-router.post("/v1/addAvailability", authenticate, addAvailabilityCon);
+router.post("/v1/addAvailability", authenticate, protect , addAvailabilityCon);
 router.post("/v1/getAvailabilityWithUser", authenticate , protect , getAvailabilityWTCon);
 
 
